5af6ffd6fde3829c595fd62c1e14a5f39c8847d7
Marco <marcofusillo99@outlook.com>
2020-10-01 13:10:38 +0200

    Implementazione  del metodo pred

--- Map7Client_ext/src/LoadController.java
+++ Map7Client_ext/src/LoadController.java
@@ -4,12 +4,21 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.print.DocFlavor.STRING;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
@@ -22,16 +31,20 @@ public class LoadController {
 	@FXML
 	private Button btnPredPhase;
 
-	@FXML
-	private TextField txtChoise;
 
 	@FXML
 	private Button btnSubmit;
 
+    @FXML
+    private ComboBox<String> cmbxChoiseBranch;
+
+
 	Socket socket = ConnectedController.socket;
 	ObjectOutputStream out = ConnectedController.out;
 	ObjectInputStream in = ConnectedController.in;
+	final String regularEx=new String("[0-9]+:(.*)");
 
+	
 	public void printRules() {
 		String answer = "";
 
@@ -103,69 +116,73 @@ public class LoadController {
 	@FXML
 	void predictionPhase(ActionEvent event) {
 		String answer = "";
-		txtChoise.setVisible(true);
+		btnPredPhase.setDisable(true);
+		cmbxChoiseBranch.setVisible(true);
 		btnSubmit.setVisible(true);
-
+		
 		try {
-			answer = in.readObject().toString();
+			answer = in.readObject().toString(); // Legge ok oppure messaggio errore
 			if (!answer.equals("OK")) {
 				System.err.println(answer);
 				return;
 			}
 
-			char risp = 'y';
-			do {
-				out.writeObject(3);
-				txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + "Starting prediction phase!");
-				answer = in.readObject().toString(); // Reading tree.predictClass()
+			out.writeObject(3); // Stampo 3 per far capire al server che sto iniziando fase predizione
+			txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + "Starting prediction phase!");
+			answer = in.readObject().toString(); // Reading tree.predictClass() from server
 
-				while (answer.equals("QUERY")) {
-					// Formulating query, reading answer
-					answer = in.readObject().toString(); // Read trees
-					txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + answer);
-					choiceBranch();
-
-//						int path = Integer.parseInt(txtChoise.getText());
-//						out.writeObject(path);
-//						answer = in.readObject().toString();
-//					}
-				}
-//					int path = Keyboard.readInt();
-//					out.writeObject(path);
-//					answer = in.readObject().toString();
-//				}
-//
-//				if (answer.equals("OK")) { // Reading prediction
-//					answer = in.readObject().toString();
-//					txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + "Predicted class:"+answer);
-//				} else // Printing error message
-//					System.err.println(answer);
-//
-//				do {
-//					txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + 	"Do you want to repeat? (y/n)");
-//					risp = Keyboard.readChar();
-//				} while (Character.toUpperCase(risp) != 'Y' && Character.toUpperCase(risp) != 'N');
-
-			} while (Character.toUpperCase(risp) == 'Y');
-
-			// Aggiunta stampa per far capire al server che l'esecuzione del client vuole
-			// terminare
-
-			out.writeObject(0);
-			System.out.println("Thank you for having used this Regression Tree Learner! See you soon...");
+			if(answer.equals("QUERY")) {
+				answer = in.readObject().toString(); // Read trees
+				txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + answer);
+				setComboItem(countBranches(answer));
+			}else {
+				//print error
+			}
+				
 
 		} catch (IOException | ClassNotFoundException e) {
 			System.out.println(e.toString());
-		} finally {
-			try {
-				socket.close();
-			} catch (IOException e1) {
-				System.err.println("[!] Error [!] Socket has not been closed correctly.");
-
-			}
-			System.out.println("\n \n \nPress Any Key To Exit...");
-		}
+		} 
+//		finally {
+//			try {
+//				socket.close();
+//			} catch (IOException e1) {
+//				System.err.println("[!] Error [!] Socket has not been closed correctly.");
+//
+//			}
+//			System.out.println("\n \n \nPress Any Key To Exit...");
+//		}
 	}
+    @FXML
+    void submitChoice(ActionEvent event) throws IOException {
+    	String elementSelected = cmbxChoiseBranch.getSelectionModel().getSelectedItem();
+    	int path = Integer.parseInt(elementSelected);
+    	out.writeObject(path);	
+    	
+    	String answer;
+		try {
+			answer = in.readObject().toString();
+			if(answer.equalsIgnoreCase("QUERY")) {
+				answer = in.readObject().toString();
+				txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + answer);
+				setComboItem(countBranches(answer));
+			} else if (answer.equalsIgnoreCase("OK")) {
+				answer = in.readObject().toString();
+				txtAreaLoad.setText(txtAreaLoad.getText() + "\n" + "Predicted class:"+answer);
+				repeatPrediction();
+				
+			} else {
+				//verificare l'header error 
+				printError("Error", "??", answer);
+			}
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Read trees
+
+    }
 
 	public void printError(String title, String headerText, String contentText) {
 
@@ -178,6 +195,29 @@ public class LoadController {
 
 		alert.showAndWait();
 	}
+	private void setComboItem(int numberBranch) {
+		
+		String[] items=new String[numberBranch];
+		for(int i=0;i<numberBranch;i++) {
+			items[i]=""+i;
+		}
+		ObservableList<String> list = FXCollections.observableArrayList(items);
+		cmbxChoiseBranch.setItems(list);
+		
+	}
+	private int countBranches(String toMaches) {
+		
+		Pattern branch = Pattern.compile(regularEx);
+		Matcher countBranch = branch.matcher(toMaches); 
+		int count = 0;
+		
+		while (countBranch.find()) {
+		    count++;
+		}
+
+		return count;
+		
+	}
 	public void choiceBranch() {
 		TextInputDialog dlg = new TextInputDialog();
 		dlg.setX(250);
@@ -187,8 +227,32 @@ public class LoadController {
 		Optional<String> result =dlg.showAndWait();
 		TextField input =dlg.getEditor();
 		final Button btn = (Button) dlg.getDialogPane().getButtonTypes();
-		//DA FINIRE QUANDO SI CLICCA OK BISOGNA FAR RESTITUIRE il path
+
 
 	}
+	private void repeatPrediction() {
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		
+		alert.setContentText("Would you repeat the prediction ?");
+		ButtonType okButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+		ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+		alert.getButtonTypes().setAll(okButton, noButton);
+		alert.showAndWait().ifPresent(type -> {
+		        if (type == ButtonType.OK) {
+		        	btnPredPhase.setDisable(false);
+		        	predictionPhase(null);
+		        } else if (type == ButtonType.NO) {
+		        	try {
+		        		alert.setContentText("Thank you for having used this Regression Tree Learner! See you soon...");
+						out.writeObject(0);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        	
+		        }
+		});
+	}
+
 
 }
